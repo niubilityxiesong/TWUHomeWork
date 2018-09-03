bf30452522c01dc690791314afa2f8cf211660b3
niubilityxiesong <563527588@qq.com>
2018-09-03 22:05:02 +0800

    [Song XIE]feat: Draw a vertical line

---
+++ src/DrawVerticalLine.java
@@ -0,0 +1,7 @@
+public class DrawVerticalLine {
+    public static void verticalLine(int number) {
+        for (int i = 0; i < number; i++) {
+            System.out.println("*");
+        }
+    }
+}
